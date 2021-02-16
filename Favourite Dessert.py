00da6ce0de6aefae3369168e718521c43db8b0d0
gnehciak <lkc364636722@gmail.com>
2021-02-17 10:33:54 +1100

    2021年2月17日

--- Favourite Dessert.py
+++ Favourite Dessert.py
@@ -1,13 +1,32 @@
-flavours = {}
-line = input('name and flavour: ')
+foods = {}
+line = input('Name:vote ')
 while line:
-  name, flavour = line.split()
-  if flavour not in flavours:
-    # add it to our dictionary as a list with one element
-    flavours[flavour] = [name]
+  name, food = line.split(':')
+  if food not in foods:
+    foods[food] = [1, name]
   else:
-    flavours[flavour].append(name)
-  line = input('name and flavour: ')
+    foods[food].append(name)
+    foods[food][0] += 1
+  line = input('Name:vote ')
 
-for flavour in flavours:
-  print(flavour, ':', ' '.join(flavours[flavour]))
+for food in foods:
+  print(f"{food} {foods[food][0]} vote(s): {' '.join(foods[food][1:])}")
+
+"""
+
+Name:vote Greg:chocolate
+Name:vote Teena:macaroons
+Name:vote Georgina:apple pie
+Name:vote Will:chocolate
+Name:vote Sophia:gelato
+Name:vote Sam:ice cream
+Name:vote James:chocolate
+Name:vote Kirsten:gelato
+Name:vote 
+apple pie 1 vote(s): Georgina
+gelato 2 vote(s): Sophia Kirsten
+chocolate 3 vote(s): Greg Will James
+macaroons 1 vote(s): Teena
+ice cream 1 vote(s): Sam
+
+"""
