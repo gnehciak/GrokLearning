018a090807a379b1efce68fb8aad18058ce347e5
gnehciak <lkc364636722@gmail.com>
2021-02-16 22:52:36 +1100

    2021年2月16日

--- X marks the trail.py
+++ X marks the trail.py
@@ -2,10 +2,7 @@ size = int(input('Grid size: '))
 
 table = []
 for i in range(size):
-  row = []
-  for j in range(size):
-    row.append('.')
-  table.append(row)
-
-for row in table:
-  print(row)
+    row = []
+    for j in range(size):
+        row.append('.')
+    table.append(row)
