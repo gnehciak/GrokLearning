162949367146fb905a87c5fc0114aa243f70daaf
gnehciak <lkc364636722@gmail.com>
2021-02-17 09:05:27 +1100

    finishe

--- X marks the trail.py
+++ X marks the trail.py
@@ -1,5 +1,6 @@
 size = int(input('Grid size: '))
 
+pos = [0, 0]
 table = []
 for i in range(size):
     row = []
@@ -7,4 +8,20 @@ for i in range(size):
         row.append('.')
     table.append(row)
 
-#Testinnngggg
+line = ' '
+while line:
+    if(line == 'right'):
+        pos[1] += 1
+    elif(line == 'down'):
+        pos[0] += 1
+    elif(line == 'left'):
+        pos[1] -= 1
+    elif(line == 'up'):
+        pos[0] -= 1
+    table[pos[0]][pos[1]] = 'x'
+
+    for row in table:
+        for i in row:
+            print(i, end='')
+        print()
+    line = input('Direction: ')
