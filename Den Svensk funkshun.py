a7f0189a1c0dae89f64c06ca34986ba8fb41c5b9
gnehciak <lkc364636722@gmail.com>
2021-02-17 16:25:14 +1100

    2021年2月17日

--- Den Svensk funkshun.py
+++ Den Svensk funkshun.py
@@ -4,10 +4,12 @@ def eng2chef(line):
     translate = {'tion': 'shun', 'an': 'un', 'th':'z','v':'f','w':'v','c':'k','o':'oo','i':'ee',}
     for value in translate.keys():
         line = line.replace(value, translate[value])
-    line = line.replace('!', '. bork bork bork!!')
+    if(line[-1] == '!'):
+        line = line.replace('!', '. bork bork bork!!')
     return line
 
 print(eng2chef('action!'))
+
 
 
 
