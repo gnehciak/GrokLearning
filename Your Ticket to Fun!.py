81b3cbd5faa54a9f464008e0e9fc8dd4396461aa
gnehciak <lkc364636722@gmail.com>
2021-03-09 22:41:47 +1100

    2021年3月9日

--- Your Ticket to Fun!.py
+++ Your Ticket to Fun!.py
@@ -1,5 +1,37 @@
 def buy_slinkies(tickets):
-# Finish the function to convert tickets to slinkies
+    return int(tickets / 3)
 
 
-# Write the rest of your program here
+ctest = {}
+
+# Get Names
+
+print("Who's here at the carnival today?")
+
+while True:
+
+    name = input("Name: ")
+    if not name:
+        break
+    ticket = int(input("Starting tickets: "))
+    if name not in ctest:
+        ctest[name] = ticket
+    print(f"{name}'s here, starting with {buy_slinkies(ticket)} slinkies worth of tickets!")
+
+# Begin
+
+print("Let the games begin!")
+while True:
+    name = input("Who played? ")
+    if not name:
+        break
+    if name not in ctest:
+        ctest[name] = 0
+    ticket = int(input("Tickets won/lost: "))
+    ctest[name] += ticket
+
+#End
+
+print("End of the day! Let's see how everyone did!")
+for i in ctest:
+    print(i, "can buy", buy_slinkies(ctest[i]), "slinkies.")
