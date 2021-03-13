cee864d170dc86931b2f6ed0f193ec65a806a444
gnehciak <lkc364636722@gmail.com>
2021-03-13 21:45:15 +1100

    shortened

--- Your Ticket to Fun!.py
+++ Your Ticket to Fun!.py
@@ -1,37 +1,31 @@
 def buy_slinkies(tickets):
     return int(tickets / 3)
 
-
 ctest = {}
 
-# Get Names
-
 print("Who's here at the carnival today?")
-
-while True:
-
+name = input("Name: ")
+while name:
+    ctest[name] = int(input("Starting tickets: ")) if name not in ctest else None
+    print(f"{name}'s here, starting with {buy_slinkies(ctest[name])} slinkies worth of tickets!")
     name = input("Name: ")
-    if not name:
-        break
-    ticket = int(input("Starting tickets: "))
-    if name not in ctest:
-        ctest[name] = ticket
-    print(f"{name}'s here, starting with {buy_slinkies(ticket)} slinkies worth of tickets!")
-
-# Begin
 
 print("Let the games begin!")
-while True:
+name = input("Who played? ")
+while name:
+    ctest[name] = int(input("Tickets won/lost: ")) if name not in ctest else ctest[name] + int(input("Tickets won/lost: "))
     name = input("Who played? ")
-    if not name:
-        break
-    if name not in ctest:
-        ctest[name] = 0
-    ticket = int(input("Tickets won/lost: "))
-    ctest[name] += ticket
-
-#End
 
 print("End of the day! Let's see how everyone did!")
 for i in ctest:
     print(i, "can buy", buy_slinkies(ctest[i]), "slinkies.")
+
+
+
+
+
+
+
+
+
+
